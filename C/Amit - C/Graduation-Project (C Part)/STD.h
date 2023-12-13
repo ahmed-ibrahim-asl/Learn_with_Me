3f0456adbc82c1a9247ca84e6be4d7afd7a89b43
mr_sam <aassal950@gmail.com>
2023-12-14 00:07:15 +0200

    C_Project 0x01

--- C/Amit - C/Graduation-Project (C Part)/STD.h
+++ C/Amit - C/Graduation-Project (C Part)/STD.h
@@ -9,6 +9,11 @@ typedef unsigned short uint16;
 typedef short int16;
 typedef unsigned int uint32;
 typedef int int32;
-//! typedef enum { false, true } bool;
+
+typedef enum
+{
+    false,
+    true
+} bool;
 
 #endif
---
+++ C/Amit - C/Graduation-Project (C Part)/main.c
@@ -0,0 +1,11 @@
+/* Include Section start */
+#include <stdio.h>
+#include <stdlib.h>
+#include "SDB.h"
+/* Include Section stop */
+
+
+
+int main(){
+SDB_APP();
+}
